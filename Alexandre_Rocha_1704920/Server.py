bafd4fbbe4d16a6a9742f76f55bb58ae6f019080
Alexandre Rossa Rocha <alex1pap2020@gmail.com>
2024-05-02 22:33:03 +0100

    Alterações

--- Alexandre_Rocha_1704920/Server.py
+++ Alexandre_Rocha_1704920/Server.py
@@ -13,4 +13,5 @@ def balance(address):
     balance = get_balance(address)
     return jsonify({'address': address, 'balance': balance})
 
-import requests
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
